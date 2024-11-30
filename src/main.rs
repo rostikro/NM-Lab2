68d2383325d60b40d5e44d874468548c8e61312c
Rozerchik <rostikigri@gmail.com>
2024-11-30 16:31:52 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use crate::simple_iteration::simple_iteration;
+use crate::square_roots_method::square_root_method;
+
+mod simple_iteration;
+mod square_roots_method;
+
+fn main() {
+    //simple_iteration();
+    square_root_method();
+}
